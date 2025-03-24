308b07c5e8a9642a24f5ee27e9f99b6bd68168ad
yukicoder0509 <bendoris0509@gmail.com>
2025-03-24 20:26:21 +0800

    feat: use migration config as schema

--- backend/internal/database/migrations/user_migration.up.sql
+++ backend/internal/database/migrations/user_migration.up.sql
@@ -5,11 +5,3 @@ CREATE TABLE IF NOT EXISTS users (
     name VARCHAR(30) NOT NULL,
     password VARCHAR(200) NOT NULL
 );
-
-CREATE TABLE IF NOT EXISTS posts (
-    id UUID PRIMARY KEY,
-    author_id UUID,
-    title VARCHAR(200),
-    content TEXT,
-    create_at TIMESTAMPTZ DEFAULT now()
-);
