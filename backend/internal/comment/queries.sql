6d8d165106bd28d810ad1a89a744ca03ac300cca
yukicoder0509 <bendoris0509@gmail.com>
2025-03-28 22:01:08 +0800

    feat: add comment SQL queries

---
+++ backend/internal/comment/queries.sql
@@ -0,0 +1,14 @@
+-- name: FindAll :many
+SELECT * FROM comments;
+
+-- name: FindByID :one
+SELECT * FROM comments WHERE id = $1;
+
+-- name: Create :one
+INSERT INTO comments (post_id, author_id, title, content) RETURNING *;
+
+-- name: Update :one
+UPDATE comments SET title = $2, content = $3 WHERE id = $1 RETURNING *;
+
+-- name: Delete :one
+DELETE FROM comments WHERE id = $1;
