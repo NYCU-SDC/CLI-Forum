dd5e72c4ea4ebb5bbeb22a712d74a1a46e4a869b
yukicoder0509 <bendoris0509@gmail.com>
2025-03-30 17:11:27 +0800

    feat: add comment service CRUD unit test

--- backend/internal/comment/service_test.go
+++ backend/internal/comment/service_test.go
@@ -7,6 +7,7 @@ import (
 	"errors"
 	"github.com/jackc/pgx/v5/pgtype"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
 	"go.uber.org/zap"
 	"testing"
 	"time"
@@ -32,6 +33,9 @@ func MustParseTime(t *testing.T, src time.Time) pgtype.Timestamptz {
 
 func TestService_Create(t *testing.T) {
 	// create a test case
+	fixedTime := time.Date(2023, 10, 10, 10, 0, 0, 0, time.UTC)
+	errCreate := errors.New("error creating comment")
+
 	testCases := []struct {
 		name      string
 		data      comment.CreateParams
@@ -49,13 +53,13 @@ func TestService_Create(t *testing.T) {
 			AuthorID:  MustParseUUID(t, "00000000-0000-0000-0000-000000000002"),
 			Title:     "Test Title",
 			Content:   pgtype.Text{String: "Test Content", Valid: true},
-			CreatedAt: MustParseTime(t, time.Now()),
+			CreatedAt: MustParseTime(t, fixedTime),
 		}, nil},
 		{"Error occur when creating comment", comment.CreateParams{
 			PostID:   MustParseUUID(t, "00000000-0000-0000-0000-000000000111"),
 			AuthorID: MustParseUUID(t, "00000000-0000-0000-0000-000000000112"),
 		},
-			comment.Comment{}, errors.New("error creating comment")},
+			comment.Comment{}, errCreate},
 	}
 
 	// create a service with a mock querier
@@ -63,20 +67,206 @@ func TestService_Create(t *testing.T) {
 	service := comment.NewService(zap.NewExample(), querier)
 
 	// set up the mock expectation
-	querier.On("Create", context.Background(), testCases[0].data).Return(comment.Comment{
-		ID:        MustParseUUID(t, "00000000-0000-0000-0000-000000000003"),
-		PostID:    testCases[0].data.PostID,
-		AuthorID:  testCases[0].data.AuthorID,
-		Title:     testCases[0].data.Title,
-		Content:   testCases[0].data.Content,
-		CreatedAt: testCases[0].expectRes.CreatedAt,
-	}, nil).Once()
-	querier.On("Create", context.Background(), testCases[1].data).Return(comment.Comment{}, errors.New("error creating comment")).Once()
+	querier.On("Create", mock.Anything, testCases[0].data).Return(testCases[0].expectRes, nil).Once()
+	querier.On("Create", mock.Anything, testCases[1].data).Return(comment.Comment{}, errCreate).Once()
 
 	// iterate over test cases
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
 			res, err := service.Create(context.Background(), tc.data)
+			assert.Equal(t, tc.expectRes, res)
+			assert.Equal(t, tc.expectErr, err)
+		})
+	}
+}
+
+func TestService_GetAll(t *testing.T) {
+	// create test cases
+	fixedTime := time.Date(2023, 10, 10, 10, 0, 0, 0, time.UTC)
+	errGetAll := errors.New("error getting all comments")
+
+	testCases := []struct {
+		name      string
+		ctx       context.Context
+		expectRes []comment.Comment
+		expectErr error
+	}{
+		{"Correct get all comments", context.Background(), []comment.Comment{
+			{
+				ID:        MustParseUUID(t, "00000000-0000-0000-0000-000000000001"),
+				PostID:    MustParseUUID(t, "00000000-0000-0000-0000-000000000002"),
+				AuthorID:  MustParseUUID(t, "00000000-0000-0000-0000-000000000003"),
+				Title:     "Test Title 1",
+				Content:   pgtype.Text{String: "Test Content 1", Valid: true},
+				CreatedAt: MustParseTime(t, fixedTime),
+			},
+			{
+				ID:        MustParseUUID(t, "00000000-0000-0000-0000-000000000002"),
+				PostID:    MustParseUUID(t, "00000000-0000-0000-0000-000000000003"),
+				AuthorID:  MustParseUUID(t, "00000000-0000-0000-0000-000000000004"),
+				Title:     "Test Title 2",
+				Content:   pgtype.Text{String: "Test Content 2", Valid: true},
+				CreatedAt: MustParseTime(t, fixedTime),
+			},
+		}, nil,
+		},
+		{"Error occur when getting all comments", context.Background(), nil, errGetAll},
+	}
+
+	// create a service with a mock querier
+	querier := mocks.NewQuerier(t)
+	service := comment.NewService(zap.NewExample(), querier)
+
+	// set up the mock expectation
+	querier.On("FindAll", testCases[0].ctx).Return(testCases[0].expectRes, nil).Once()
+	querier.On("FindAll", testCases[1].ctx).Return([]comment.Comment{}, errGetAll).Once()
+
+	// iterate over test cases
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			res, err := service.GetAll(tc.ctx)
+			assert.Equal(t, tc.expectRes, res)
+			assert.Equal(t, tc.expectErr, err)
+		})
+	}
+}
+
+func TestService_GetById(t *testing.T) {
+	// create test cases
+	fixedTime := time.Date(2023, 10, 10, 10, 0, 0, 0, time.UTC)
+	errGetByID := errors.New("error getting comment by ID")
+
+	testCases := []struct {
+		name      string
+		id        pgtype.UUID
+		expectRes comment.Comment
+		expectErr error
+	}{
+		{
+			"Correct get comment by ID",
+			MustParseUUID(t, "00000000-0000-0000-0000-000000000001"),
+			comment.Comment{
+				ID:        MustParseUUID(t, "00000000-0000-0000-0000-000000000001"),
+				PostID:    MustParseUUID(t, "00000000-0000-0000-0000-000000000002"),
+				AuthorID:  MustParseUUID(t, "00000000-0000-0000-0000-000000000003"),
+				Title:     "Test Title 1",
+				Content:   pgtype.Text{String: "Test Content 1", Valid: true},
+				CreatedAt: MustParseTime(t, fixedTime),
+			}, nil,
+		},
+		{
+			"Error getting comment by ID",
+			MustParseUUID(t, "00000000-0000-0000-0000-000000000002"),
+			comment.Comment{}, errGetByID,
+		},
+	}
+
+	// create a service with a mock querier
+	querier := mocks.NewQuerier(t)
+	service := comment.NewService(zap.NewExample(), querier)
+
+	// set up the mock expectation
+	querier.On("FindByID", mock.Anything, testCases[0].id).Return(testCases[0].expectRes, nil).Once()
+	querier.On("FindByID", mock.Anything, testCases[1].id).Return(testCases[1].expectRes, errGetByID).Once()
+
+	// iterate over test cases
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			res, err := service.GetById(context.Background(), tc.id)
+			assert.Equal(t, tc.expectRes, res)
+			assert.Equal(t, tc.expectErr, err)
+		})
+	}
+}
+
+func TestService_Delete(t *testing.T) {
+	// create test cases
+	errDelete := errors.New("error getting comment by ID")
+
+	testCases := []struct {
+		name      string
+		id        pgtype.UUID
+		expectErr error
+	}{
+		{
+			"Correct delete comment",
+			MustParseUUID(t, "00000000-0000-0000-0000-000000000001"),
+			nil,
+		},
+		{
+			"Error getting comment by ID",
+			MustParseUUID(t, "00000000-0000-0000-0000-000000000002"),
+			errDelete,
+		},
+	}
+
+	// create a service with a mock querier
+	querier := mocks.NewQuerier(t)
+	service := comment.NewService(zap.NewExample(), querier)
+
+	// set up the mock expectation
+	querier.On("Delete", mock.Anything, testCases[0].id).Return(nil).Once()
+	querier.On("Delete", mock.Anything, testCases[1].id).Return(errDelete).Once()
+
+	// iterate over test cases
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			err := service.Delete(context.Background(), tc.id)
+			assert.Equal(t, tc.expectErr, err)
+		})
+	}
+}
+
+func TestService_Update(t *testing.T) {
+	// create test cases
+	fixedTime := time.Date(2023, 10, 10, 10, 0, 0, 0, time.UTC)
+	errUpdate := errors.New("error updating comment")
+
+	testCases := []struct {
+		name      string
+		data      comment.UpdateParams
+		expectRes comment.Comment
+		expectErr error
+	}{
+		{
+			"Correct update comment",
+			comment.UpdateParams{
+				ID:      MustParseUUID(t, "00000000-0000-0000-0000-000000000001"),
+				Title:   "Updated Title",
+				Content: pgtype.Text{String: "Updated Content", Valid: true},
+			},
+			comment.Comment{
+				ID:        MustParseUUID(t, "00000000-0000-0000-0000-000000000001"),
+				PostID:    MustParseUUID(t, "00000000-0000-0000-0000-000000000002"),
+				AuthorID:  MustParseUUID(t, "00000000-0000-0000-0000-000000000003"),
+				Title:     "Updated Title",
+				Content:   pgtype.Text{String: "Updated Content", Valid: true},
+				CreatedAt: MustParseTime(t, fixedTime),
+			}, nil,
+		},
+		{
+			"Error updating comment",
+			comment.UpdateParams{
+				ID:      MustParseUUID(t, "00000000-0000-0000-0000-000000000001"),
+				Title:   "Updated Title",
+				Content: pgtype.Text{String: "Updated Content", Valid: true},
+			},
+			comment.Comment{}, errUpdate,
+		},
+	}
+
+	// create a service with a mock querier
+	querier := mocks.NewQuerier(t)
+	service := comment.NewService(zap.NewExample(), querier)
+
+	// set up the mock expectation
+	querier.On("Update", mock.Anything, testCases[0].data).Return(testCases[0].expectRes, nil).Once()
+	querier.On("Update", mock.Anything, testCases[1].data).Return(testCases[1].expectRes, errUpdate).Once()
+
+	// iterate over test cases
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			res, err := service.Update(context.Background(), tc.data)
 			assert.Equal(t, tc.expectRes, res)
 			assert.Equal(t, tc.expectErr, err)
 		})
