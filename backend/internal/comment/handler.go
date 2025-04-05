da357a31b536dc1c522760cefba60dcbec21c5bb
yukicoder0509 <bendoris0509@gmail.com>
2025-04-05 14:32:09 +0800

    feat: add get comment by post id handler

--- backend/internal/comment/handler.go
+++ backend/internal/comment/handler.go
@@ -15,6 +15,7 @@ import (
 type Getter interface {
 	GetAll(ctx context.Context) ([]Comment, error)
 	GetById(ctx context.Context, id pgtype.UUID) (Comment, error)
+	GetByPost(ctx context.Context, postId pgtype.UUID) ([]Comment, error)
 }
 
 type Store interface {
@@ -42,6 +43,10 @@ func NewHandler(logger *zap.Logger, getter Getter, store Store) *Handler {
 
 type GetByIdRequest struct {
 	ID string `json:"id" validate:"required,uuid"`
+}
+
+type GetByPostRequest struct {
+	PostId string `json:"post_id" validate:"required,uuid"`
 }
 
 type Response struct {
@@ -124,6 +129,54 @@ func (h *Handler) GetCommentByIdHandler(w http.ResponseWriter, r *http.Request) 
 		Title:     comment.Title.String,
 		Content:   comment.Content.String,
 		CreatedAt: comment.CreatedAt.Time.String(),
+	}
+
+	// Write response
+	internal.WriteJSONResponse(w, http.StatusOK, response)
+}
+
+func (h *Handler) GetCommentByPostHandler(w http.ResponseWriter, r *http.Request) {
+	traceCtx, span := h.tracer.Start(r.Context(), "GetCommentByPostEndpoint")
+	defer span.End()
+	logger := internal.LoggerWithContext(traceCtx, h.logger)
+
+	// Extract PostID from request
+	var req GetByPostRequest
+	err := internal.ParseAndValidateRequestBody(traceCtx, h.validator, r, &req)
+	if err != nil {
+		logger.Error("Error decoding request body", zap.Error(err), zap.Any("body", r.Body))
+		problem.WriteError(traceCtx, w, err, logger)
+		return
+	}
+
+	// Verify and transform PostID
+	var uuid pgtype.UUID
+	err = uuid.Scan(req.PostId)
+	if err != nil {
+		logger.Error("Error parsing UUID", zap.Error(err), zap.String("post_id", req.PostId))
+		problem.WriteError(traceCtx, w, err, logger)
+		return
+	}
+
+	// Fetch comments by PostID
+	var response []Response
+	comments, err := h.getter.GetByPost(r.Context(), uuid)
+	if err != nil {
+		logger.Error("Error fetching comments by post id", zap.Error(err), zap.String("post_id", req.PostId))
+		problem.WriteError(traceCtx, w, err, logger)
+		return
+	}
+
+	// Convert comments to Response
+	for _, comment := range comments {
+		response = append(response, Response{
+			ID:        comment.ID.String(),
+			PostId:    comment.PostID.String(),
+			AuthorId:  comment.AuthorID.String(),
+			Title:     comment.Title.String,
+			Content:   comment.Content.String,
+			CreatedAt: comment.CreatedAt.Time.String(),
+		})
 	}
 
 	// Write response
